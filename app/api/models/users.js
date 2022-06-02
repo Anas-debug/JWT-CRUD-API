17b901e87adc65eda29a77de1d9ad345e07b3e77
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-06-02 13:34:28 +0100

    updated server.js set up models, routes and controllers

--- app/api/models/users.js
+++ app/api/models/users.js
@@ -1,29 +1,28 @@
-const userModel = require('../models/users');
-const bcrypt = require('bcrypt'); 
-const jwt = require('jsonwebtoken');
-module.exports = {
- create: function(req, res, next) {
-  
-  userModel.create({ name: req.body.name, email: req.body.email, password: req.body.password }, function (err, result) {
-      if (err) 
-       next(err);
-      else
-       res.json({status: "success", message: "User added successfully!!!", data: null});
-      
-    });
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+//Define a schema
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+ name: {
+  type: String,
+  trim: true,  
+  required: true,
  },
-authenticate: function(req, res, next) {
-  userModel.findOne({email:req.body.email}, function(err, userInfo){
-     if (err) {
-      next(err);
-     } else {
-if(bcrypt.compareSync(req.body.password, userInfo.password)) {
-const token = jwt.sign({id: userInfo._id}, req.app.get('secretKey'), { expiresIn: '1h' });
-res.json({status:"success", message: "user found!!!", data:{user: userInfo, token:token}});
-}else{
-res.json({status:"error", message: "Invalid email/password!!!", data:null});
-}
-     }
-    });
+ email: {
+  type: String,
+  trim: true,
+  required: true
  },
-}
+ password: {
+  type: String,
+  trim: true,
+  required: true
+ }
+});
+// hash user password before saving into database
+UserSchema.pre('save', function(next){
+this.password = bcrypt.hashSync(this.password, saltRounds);
+next();
+});
+module.exports = mongoose.model('User', UserSchema);
